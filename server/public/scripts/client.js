068b432204865cb104066d0edd9c379fc8a52153
nkean <nathankean2@gmail.com>
2018-04-21 16:35:25 -0500

    client can POST calc parameters to server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,42 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('jQuery has been loaded');
+    $('#addButton').click(addNumbers);
+    $('#subtractButton').click(subtractNumbers);
+    $('#multiplyButton').click(multiplyNumbers);
+    $('#divideButton').click(divideNumbers);
+}
+
+function addNumbers() {
+    sendCalculation('add');
+}
+
+function subtractNumbers() {
+    sendCalculation('subtract');
+}
+
+function multiplyNumbers() {
+    sendCalculation('multiply');
+}
+
+function divideNumbers() {
+    sendCalculation('divide');
+}
+
+function sendCalculation (type) {
+    const calcParams = {
+        x: $('#inputNum1').val(),
+        y: $('#inputNum2').val(),
+        type: type
+    };
+    $('input').val(''); // clear input fields
+    console.log(calcParams);
+    $.ajax({
+        method: 'POST',
+        url: '/calculate',
+        data: calcParams
+    })
+        .then(function(response) {
+            console.log(response);
+        });
 }
--- server/server.js
+++ server/server.js
@@ -3,8 +3,15 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+calcHistory = [];
+
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/calculate', (req, res) => {
+    console.log(req.body);
+    res.sendStatus(200);
+});
 
 app.listen(PORT, () => {
     console.log(`server up on port: ${PORT}`)
