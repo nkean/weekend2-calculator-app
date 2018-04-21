8f9bf3201bef03f3c316624924056b501b6de950
nkean <nathankean2@gmail.com>
2018-04-21 17:07:36 -0500

    calculates answer and stores

--- server/server.js
+++ server/server.js
@@ -3,13 +3,19 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const calculate = require('./modules/calculate');
 calcHistory = [];
 
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.get('/history', (req,res) => {
+    res.send(calcHistory);
+});
+
 app.post('/calculate', (req, res) => {
-    console.log(req.body);
+    let newCalc = calculate(req.body);
+    calcHistory.push(newCalc);
     res.sendStatus(200);
 });
 
